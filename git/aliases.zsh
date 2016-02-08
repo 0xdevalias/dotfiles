868aa212f00c1d1ab954b79aed24c0d834e532c4
Glenn 'devalias' Grant <glenn@ensquared.net>
2016-02-08 11:58:43 +1100

    Improve git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,8 +7,11 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
+#alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl1="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gl2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+alias gl="gl1"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
