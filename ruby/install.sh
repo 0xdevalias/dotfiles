6a3a47ee6188aa7a80e69f5c15c34b04850cef3e
Glenn 'devalias' Grant <glenn@ensquared.net>
2016-02-08 11:44:48 +1100

    Install/set global ruby and default gems

--- ruby/install.sh
+++ ruby/install.sh
@@ -11,3 +11,12 @@ then
   echo "  Installing ruby-build for you."
   brew install ruby-build > /tmp/ruby-build-install.log
 fi
+
+rbenv install 2.3.0
+
+rbenv rehash
+
+rbenv global 2.3.0
+
+gem install bundler
+gem install jekyll
--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,5 +1,6 @@
 # init according to man page
 if (( $+commands[rbenv] ))
 then
+  echo "Loading rbenv.."
   eval "$(rbenv init -)"
 fi
