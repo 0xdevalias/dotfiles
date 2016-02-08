0bde091236e137e76e8b69f89fae11c6132acc29
Glenn 'devalias' Grant <glenn@ensquared.net>
2016-02-08 12:00:59 +1100

    Add ssh aliases & ssh-keyinit helper

---
+++ ssh/aliases.zsh
@@ -0,0 +1,7 @@
+### SSH
+alias ssh-list="cat ~/.ssh/config | grep \"Host \""
+
+### SSHFS
+#alias unsshfs="umount -f ~/mnt/ssh/"
+#alias sshfs-wlpc-staging="sshfs wlpc-staging:/ ~/mnt/wlpc && sleep 1 && cd ~/mnt/wlpc && pwd && ls"
+#alias unsshfs-wlpc="umount -f ~/mnt/wlpc/ && ls ~/mnt/wlpc"
