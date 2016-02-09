221d085054792eaa8acbb3f602734c76f11a46ff
Glenn 'devalias' Grant <glenn@ensquared.net>
2016-02-09 14:49:36 +1100

    Make homebrew aliases more robust

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,5 +1,6 @@
 # Homebrew
 alias brew-dir="cd $(brew --prefix) && pwd"
-alias brew-cellardir="cd $(brew --prefix)/Cellar && pwd"
-alias brew-cachedir="cd /Library/Caches/Homebrew && pwd"
+alias brew-cellardir="cd $(brew --cellar) && pwd"
+alias brew-cachedir="cd $(brew --cache) && pwd"
 alias brew-caskdir="cd $(brew --prefix)/Library/Taps/caskroom/homebrew-cask/ && pwd"
+alias brew-commands="brew commands"
