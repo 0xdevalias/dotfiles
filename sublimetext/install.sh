9931d6fe9250a139705aeb3ae01dab0b00ae3b95
Glenn 'devalias' Grant <glenn@devalias.net>
2016-05-09 11:01:08 +1000

    Add Sublime Text 3 user preferences

---
+++ sublimetext/install.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+ST_USERPATH="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+ST_SETTINGSPATH="$ST_USERPATH/Preferences.sublime-settings"
+ST_DOTFILEPATH="$ZSH/sublimetext/Preferences.sublime-settings"
+
+# Make settings folder if it doesn't exist
+if [[ ! -d "$ST_USERPATH" ]]; then
+  echo "[sublimetext] Making settings folder.. ($ST_USERPATH)"
+  mkdir -p "$ST_USERPATH"
+fi
+
+# If file exists already, move into dotfiles (git will track differences)
+if [[ -f "$ST_SETTINGSPATH" ]] && [[ ! -h "$ST_SETTINGSPATH" ]]; then
+  echo "[sublimetext] User settings found, moving into dotfiles.. ($ST_DOTFILEPATH)"
+  mv "$ST_SETTINGSPATH" "$ST_DOTFILEPATH"
+fi
+
+if [[ ! -L "$ST_SETTINGSPATH" ]]; then
+  echo "[sublimetext] Symlinking settings path to dotfiles.. ($ST_SETTINGSPATH)"
+  ln -s "$ST_DOTFILEPATH" "$ST_SETTINGSPATH"
+else
+  echo "[sublimetext] Everything looks good here. Nothing to setup."
+fi
