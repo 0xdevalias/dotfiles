300f2cdd92703d1c1210107053e292fbd8b5576c
Glenn 'devalias' Grant <glenn@devalias.net>
2018-05-26 10:12:15 +1000

    [bin] Add 'cdgo-helper' + 'cdgo/cdnogo' aliases

--- localdev/aliases.zsh
+++ localdev/aliases.zsh
@@ -1,2 +1,4 @@
 alias localdev='source ~/.dotfiles/bin/localdev-helper'
 alias cdln='source ~/.dotfiles/bin/cdln-helper'
+alias cdgo='source ~/.dotfiles/bin/cdgo-helper GO'
+alias cdnogo='source ~/.dotfiles/bin/cdgo-helper NOGO'
