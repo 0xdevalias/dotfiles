112ed88eb97ab4a58eb2358a8f18734ae7db2e86
Glenn 'devalias' Grant <glenn@devalias.net>
2018-05-26 10:31:29 +1000

    [bin] Allow cdgo-helper/cdgo/cdnogo to change path at destination as well

--- localdev/aliases.zsh
+++ localdev/aliases.zsh
@@ -1,4 +1,4 @@
 alias localdev='source ~/.dotfiles/bin/localdev-helper'
 alias cdln='source ~/.dotfiles/bin/cdln-helper'
-alias cdgo='source ~/.dotfiles/bin/cdgo-helper GO'
-alias cdnogo='source ~/.dotfiles/bin/cdgo-helper NOGO'
+alias cdgo='source ~/.dotfiles/bin/cdgo-helper GO $1'
+alias cdnogo='source ~/.dotfiles/bin/cdgo-helper NOGO $1'
