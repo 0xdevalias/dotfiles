a6ebac49da7e0c7e82d1b62cbdc51e1a5f2a6059
Glenn 'devalias' Grant <glenn@devalias.net>
2018-10-08 12:15:45 +1100

    [node] Add/update aliases for list, outdated, check, etc

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -1,1 +1,8 @@
-alias npm-global="npm list -g --depth=0"
+alias npm-list="npm list --depth=0"
+alias npm-list-global="npm list -g --depth=0"
+
+alias npm-outdated="npm outdated"
+alias npm-outdated-global="npm outdated -g"
+
+# https://github.com/dylang/npm-check
+alias npm-check="npx npm-check"
