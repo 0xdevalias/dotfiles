f46631d1bf4c755cfd9e02857943a374e9bf6ffe
Glenn 'devalias' Grant <glenn@devalias.net>
2018-12-06 08:17:49 +1100

    Fix PATH, ./bin can cause infinite loops of pain

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="$ZSH/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
