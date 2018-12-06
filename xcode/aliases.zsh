f68ce43c965b11ce819bd8ba8b551827d36e4694
Glenn 'devalias' Grant <glenn@devalias.net>
2018-12-06 15:11:40 +1100

    [xcode] alias filemerge

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,1 +1,3 @@
+# TODO: Would these be better as binstubs with instructions on installing xcode when it's missing?
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+alias filemerge="open /Applications/Xcode.app/Contents/Applications/FileMerge.app"
