60c969a5c1e919c511552c1c6032a58efc6e51e8
Glenn 'devalias' Grant <glenn@devalias.net>
2019-05-19 09:06:13 +1000

    [asdf] Fix PATH

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -2,6 +2,6 @@
 if (( $+commands[asdf] ))
 then
   echo "Loading asdf.."
-  export ASDF_DATA_DIR=`brew --prefix asdf`/
+  export ASDF_DATA_DIR=`brew --prefix asdf`
   source $ASDF_DATA_DIR/asdf.sh
 fi
