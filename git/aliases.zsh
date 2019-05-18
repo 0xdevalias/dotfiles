b38e873ad77d001ce8401e2d0fd631ac11554d54
Glenn 'devalias' Grant <glenn@devalias.net>
2019-05-19 09:10:00 +1000

    [git] Add CLI alias for GitKraken

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,6 +6,8 @@ then
   alias git=$hub_path
 fi
 
+alias kraken='open -na GitKraken --args -p "$(git rev-parse --show-toplevel)"'
+
 # The rest of my fun git aliases
 #alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
