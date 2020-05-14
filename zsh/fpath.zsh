82291c4b613d4d11f405dd6c8d96767242850c52
Glenn 'devalias' Grant <glenn@devalias.net>
2020-05-14 16:34:44 +1000

    [zsh] cleanup completions loading, etc

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,11 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+fpath=($ZSH/functions $fpath)
+
+echo Loading completions..
+for file in $ZSH/functions/*(:t)
+do
+  echo "  $file"
+done
+autoload -U $ZSH/functions/*(:t)
