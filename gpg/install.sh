25b24b09aa08b55546d9f7939359b4db9d109647
Glenn 'devalias' Grant <glenn@devalias.net>
2020-05-31 15:15:54 +1000

    [gpg] add install script

---
+++ gpg/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+# Import our common helper scripts
+source "${ZSH}/lib/_helpers"
+
+echo "[gpg::install]"
+
+require_installed_brew "gpg"
+require_installed_brew "pinentry-mac"
+
+# Ensure correct permissions are set (avoids gpg: WARNING: unsafe permissions on homedir '/Users/devalias/.gnupg')
+#   https://superuser.com/questions/954509/what-are-the-correct-permissions-for-the-gnupg-enclosing-folder-gpg-warning/954536#954536
+echo "[gpg] Ensuring correct folder permissions are set"
+chown -R "$(whoami):$(id -gn)" ~/.gnupg/
+chmod 700 ~/.gnupg
+find ~/.gnupg -type f -exec chmod 600 {} \;
+find ~/.gnupg -type d -exec chmod 700 {} \;
