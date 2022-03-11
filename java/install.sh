4c9846d23c06cb06c1495f17cc209e096b530adc
Glenn 'devalias' Grant <glenn@devalias.net>
2022-03-11 13:18:38 +1100

    [java] update install.sh to use temurin rather than the legacy adoptopenjdk brew cask

--- java/install.sh
+++ java/install.sh
@@ -4,15 +4,21 @@
 source "${ZSH}/lib/_helpers"
 
 # See also
+#   https://adoptium.net/
+#     https://adoptium.net/faq.html#temurinName
+#       This is the project and brand name for the binaries that the Eclipse Foundation produces. You can think of these as the successor binaries to AdoptOpenJDK.
+#
 #   https://adoptopenjdk.net (find the latest release version from here)
 #     https://github.com/AdoptOpenJDK/homebrew-openjdk (contains additional versions)
 #       brew tap adoptopenjdk/openjdk
+#
 #   https://dzone.com/articles/a-guide-to-java-versions-and-features
+#
 #   brew search java
 #   brew search jdk
 
 require_installed_brew "jenv"
-require_installed_brew_cask "adoptopenjdk"
+require_installed_brew_cask "temurin"
 
 # Add java versions to jenv
 /usr/libexec/java_home | xargs jenv add
