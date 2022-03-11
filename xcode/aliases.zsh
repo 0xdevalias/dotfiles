fb6cd754b50164a99c26572aa17e26fe1a1ebea3
Glenn 'devalias' Grant <glenn@devalias.net>
2022-03-11 11:14:59 +1100

    [xcode] update path for filemerge alias

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,3 +1,3 @@
 # TODO: Would these be better as binstubs with instructions on installing xcode when it's missing?
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
-alias filemerge="open /Applications/Xcode.app/Contents/Applications/FileMerge.app"
+alias filemerge="/Applications/Xcode.app/Contents/Applications/FileMerge.app/Contents/MacOS/FileMerge"
