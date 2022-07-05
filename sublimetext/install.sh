899c4c3598d148269e468a203d06855e633ad3b1
Glenn 'devalias' Grant <glenn@devalias.net>
2022-07-05 14:40:46 +1000

    [sublimetext] refactor install.sh to symlink entire user settings folder + use sublpm to install Package Control by default

--- sublimetext/install.sh
+++ sublimetext/install.sh
@@ -7,25 +7,36 @@ echo "[sublimetext::install]"
 
 require_installed_brew_cask "sublime-text"
 
-ST_USERPATH="$HOME/Library/Application Support/Sublime Text/Packages/User"
-ST_SETTINGSPATH="$ST_USERPATH/Preferences.sublime-settings"
-ST_DOTFILEPATH="$ZSH/sublimetext/Preferences.sublime-settings"
+# https://packagecontrol.io/docs/syncing
+#   install Package Control on all machines and then to sync only the Packages/User/ folder.
+#   This folder contains the Package Control.sublime-settings file, which includes a list of all installed packages.
+#   If this file is copied to another machine, the next time Sublime Text is started, Package Control will install the
+#   correct version of any missing packages.
 
-# Make settings folder if it doesn't exist
+# ST_USERPATH="$HOME/Library/Application Support/Sublime Text/Packages/User"
+ST_USERPATH="$(sublpm --settingsPath)"
+ST_DOTFILEPATH="$ZSH/sublimetext/SublimeSettings"
+
+# Make sublime settings folder if it doesn't exist
 if [[ ! -d "$ST_USERPATH" ]]; then
   echo "[sublimetext] Making settings folder.. ($ST_USERPATH)"
   mkdir -p "$ST_USERPATH"
 fi
 
-# If file exists already, move into dotfiles (git will track differences)
-if [[ -f "$ST_SETTINGSPATH" ]] && [[ ! -h "$ST_SETTINGSPATH" ]]; then
+if [[ -d "$ST_USERPATH" ]] && [[ ! -L "$ST_USERPATH" ]]; then
   echo "[sublimetext] User settings found, moving into dotfiles.. ($ST_DOTFILEPATH)"
-  mv "$ST_SETTINGSPATH" "$ST_DOTFILEPATH"
+  mv "$ST_USERPATH" "$ST_DOTFILEPATH"
 fi
 
-if [[ ! -L "$ST_SETTINGSPATH" ]]; then
-  echo "[sublimetext] Symlinking settings path to dotfiles.. ($ST_SETTINGSPATH)"
-  ln -s "$ST_DOTFILEPATH" "$ST_SETTINGSPATH"
+if [[ ! -L "$ST_USERPATH" ]]; then
+  echo "[sublimetext] Symlinking settings path to dotfiles.. ($ST_USERPATH)"
+  ln -s "$ST_DOTFILEPATH" "$ST_USERPATH"
 else
   echo "[sublimetext] Everything looks good here. Nothing to setup."
 fi
+
+# While we could do these things manually here, lets use our sublpm helper script instead
+#   https://forum.sublimetext.com/t/installing-packages-from-the-command-line/64029/4
+#     subl --command "install_package_control"
+#     subl --command "advanced_install_package {\"packages\": \"TODO\"}"
+sublpm init
