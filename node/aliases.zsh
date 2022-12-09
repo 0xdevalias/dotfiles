316fb38f0444f6feb7a01ad2bb3fa0e4e03ea170
Glenn 'devalias' Grant <glenn@devalias.net>
2022-12-09 13:45:31 +1100

    [node] add aliases: yarn-check-cache-size, yarn-clean-cache, clean-yarn-cache

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -6,3 +6,7 @@ alias npm-outdated-global="npm outdated -g"
 
 # https://github.com/dylang/npm-check
 alias npm-check="npx npm-check"
+
+alias yarn-check-cache-size="du -sh ~/Library/Caches/Yarn"
+alias yarn-clean-cache="yarn-check-cache-size; rm -rf ~/Library/Caches/Yarn/*; yarn-check-cache-size"
+alias clean-yarn-cache="yarn-clean-cache"
