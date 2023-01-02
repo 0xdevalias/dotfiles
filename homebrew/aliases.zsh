94bc43f48fc697fb5e9143c1d9eca3bf28481080
Glenn 'devalias' Grant <glenn@devalias.net>
2023-01-02 18:37:28 +1100

    [homebrew] Fix brew-caskdir alias with updated path

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -2,5 +2,5 @@
 alias brew-dir="cd $(brew --prefix) && pwd"
 alias brew-cellardir="cd $(brew --cellar) && pwd"
 alias brew-cachedir="cd $(brew --cache) && pwd"
-alias brew-caskdir="cd $(brew --prefix)/Library/Taps/caskroom/homebrew-cask/ && pwd"
+alias brew-caskdir="cd $(brew --prefix)/Homebrew/Library/Taps/homebrew/homebrew-cask/Casks && pwd"
 alias brew-commands="brew commands"
