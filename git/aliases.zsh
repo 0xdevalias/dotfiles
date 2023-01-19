1c611932eab68b343008e11493edfe3ab7568406
Glenn 'devalias' Grant <glenn@devalias.net>
2023-01-20 10:27:39 +1100

    [git] Prevent aliasing git to hub

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,10 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
+# hub_path=$(which hub)
+# if (( $+commands[hub] ))
+# then
+#   alias git=$hub_path
+# fi
 
 alias kraken='open -na GitKraken --args -p "$(git rev-parse --show-toplevel)"'
 
