a3c7582396e162a22e009f954c3f909d436983bd
Glenn 'devalias' Grant <glenn@devalias.net>
2023-01-31 14:42:20 +1100

    [node] add alias: list-package-scripts (+ associated mnemonic aliases)

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -10,3 +10,16 @@ alias npm-check="npx npm-check"
 alias yarn-check-cache-size="du -sh ~/Library/Caches/Yarn"
 alias yarn-clean-cache="yarn-check-cache-size; rm -rf ~/Library/Caches/Yarn/*; yarn-check-cache-size"
 alias clean-yarn-cache="yarn-clean-cache"
+
+alias list-package-scripts="jq '.scripts' package.json"
+alias show-package-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
+
+alias npm-list-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
+alias npm-show-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
+alias list-npm-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
+alias show-npm-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
+
+alias yarn-list-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
+alias yarn-show-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
+alias list-yarn-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
+alias show-yarn-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
