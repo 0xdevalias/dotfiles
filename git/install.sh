dcb5e479acbc32fac36116a88e8c95c70dcb961b
Glenn 'devalias' Grant <glenn@devalias.net>
2023-04-12 16:45:18 +1000

    [git] install.sh: refactor to use our _helpers + check if npm installed before installing globals

--- git/install.sh
+++ git/install.sh
@@ -3,10 +3,16 @@
 # Import our common helper scripts
 source "${ZSH}/lib/_helpers"
 
+echo "[git::install]"
+
 check_installed brew
 
-# brew install hub                        # https://hub.github.com
-brew install gh                         # https://github.com/cli/cli
-brew install git-delete-merged-branches # https://github.com/hartwork/git-delete-merged-branches
+require_installed_brew gh                          # https://github.com/cli/cli
+require_installed_brew git-delete-merged-branches  # https://github.com/hartwork/git-delete-merged-branches
 
-npm install -g @githubnext/github-copilot-cli  # https://www.npmjs.com/package/@githubnext/github-copilot-cli
+if (( $+commands[npm] )); then
+  npm install -g @githubnext/github-copilot-cli  # https://www.npmjs.com/package/@githubnext/github-copilot-cli
+else
+  echo "  npm not found, skipping installation of packages"
+fi
+
