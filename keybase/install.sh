a74fc883594b3791802471b49e367043ea2fc80b
Glenn 'devalias' Grant <glenn@devalias.net>
2023-04-12 18:20:32 +1000

    [keybase] install.sh: add automagic keybase login

--- keybase/install.sh
+++ keybase/install.sh
@@ -8,3 +8,14 @@ echo "[keybase::install]"
 check_installed brew
 
 require_installed_brew_cask keybase  # https://keybase.io/
+
+# Temporarily add the keybase app bin to our path so the commands below can run
+PATH="$PATH:/Applications/Keybase.app/Contents/SharedSupport/bin"
+
+# Authenticate Keybase
+if (( $+commands[keybase] )); then
+  if ! keybase whoami >/dev/null 2>&1; then
+    echo "  Keybase not authenticated.. please login now"
+    keybase login
+  fi
+fi
