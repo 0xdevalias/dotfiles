063ccc68166c7b50a33a5fc72e8ad944be1f2135
Glenn 'devalias' Grant <glenn@devalias.net>
2023-04-28 16:40:55 +1000

    [osx] aliases: add show-macos-platform-uuid

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -11,3 +11,6 @@ alias cat_hosts="cat /private/etc/hosts"
 alias plainpaste="(pbpaste | pbcopy) && echo Clipboard content converted to plaintext"
 
 alias unlocktrash="unlock ~/.Trash && echo All files in trash unlocked"
+
+# Ref: https://apple.stackexchange.com/questions/342042/how-can-i-query-the-hardware-uuid-of-a-mac-programmatically-from-a-command-line/342043#342043
+alias show-macos-platform-uuid='ioreg -d2 -c IOPlatformExpertDevice | awk -F\" "/IOPlatformUUID/{print \$(NF-1)}"'
