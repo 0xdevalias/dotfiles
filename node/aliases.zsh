fab2f2e630c3993e2bfc63fafffe3273a539def2
Glenn 'devalias' Grant <glenn@devalias.net>
2023-05-03 13:57:32 +1000

    [node] aliases: add electron-* helpers

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -23,3 +23,59 @@ alias yarn-list-scripts="echo \"alias for 'list-package-scripts'\" && list-packa
 alias yarn-show-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
 alias list-yarn-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
 alias show-yarn-scripts="echo \"alias for 'list-package-scripts'\" && list-package-scripts"
+
+###########
+# Electron
+###########
+
+alias electron-find-apps='find /Applications -name "Electron Framework.framework"'
+
+# https://www.electronjs.org/docs/latest/tutorial/fuses#the-easy-way
+#   https://github.com/electron/fuses#readme
+# alias electron-fuse-check="npx @electron/fuses read --app"
+alias electron-fuse-check="function _electron_fuse_check() { npx @electron/fuses read --app \${1:?Error: Please specify path to electron app.}; }; _electron_fuse_check"
+
+alias electron-asar='npx asar'
+alias electron-asar-list="function _electron_asar_list() { npx asar list \${1:?Error: Please specify path to asar file.}; }; _electron_asar_list"
+alias electron-asar-list-is-pack="function _electron_asar_list_is_pack() { npx asar list --is-pack \${1:?Error: Please specify path to asar file.}; }; _electron_asar_list_is_pack"
+alias electron-asar-list-only-packed="function _electron_asar_list_only_packed() { npx asar list --is-pack \${1:?Error: Please specify path to asar file.} | grep -v unpack }; _electron_asar_list_only_packed"
+alias electron-asar-list-only-unpacked="function _electron_asar_list_only_unpacked() { npx asar list --is-pack \${1:?Error: Please specify path to asar file.} | grep unpack }; _electron_asar_list_only_unpacked"
+
+function _electron_run_app() {
+  # Get the app name from the first argument
+  APP_NAME=${1:?Error: Please specify name of electron application to run.}
+  shift
+
+  # Set the path to the app folder
+  APP_PATH="/Applications/${APP_NAME}.app"
+
+  # Check if the app exists in /Applications
+  if [ -d "${APP_PATH}" ]; then
+    echo "Found ${APP_NAME}.app in /Applications"
+
+    # Check if the app uses Electron
+    if [ -d "${APP_PATH}/Contents/Frameworks/Electron Framework.framework" ]; then
+      echo "${APP_NAME}.app uses Electron"
+
+      # Find the main executable and run it
+      MAIN_EXECUTABLE=$(plutil -p "${APP_PATH}/Contents/Info.plist" | awk -F'"' '/CFBundleExecutable/{print $4}')
+      if [ -n "${MAIN_EXECUTABLE}" ]; then
+        env | grep 'ELECTRON'
+
+        # Run the main app binary (passing it the args provided)
+         (set -x; "${APP_PATH}/Contents/MacOS/${MAIN_EXECUTABLE}" $@)
+      else
+          echo "Could not find main executable for ${APP_NAME}.app"
+      fi
+    else
+        echo "${APP_NAME}.app does not use Electron"
+    fi
+  else
+      echo "${APP_NAME}.app not found in /Applications"
+  fi
+}
+alias electron-run-app="_electron_run_app"
+alias electron-run-app-with-inspect-main="function _electron_run_app_with_inspect_main() { _electron_run_app \${1:?Error: Please specify name of electron application to run.} --inspect-brk=1337; }; _electron_run_app_with_inspect_main"
+alias electron-run-app-as-node="ELECTRON_RUN_AS_NODE=1 _electron_run_app"
+
+alias electron-run-beeper-localdev="_electron_run_app Beeper localdev localapi"
