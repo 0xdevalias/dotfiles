5e3b1a00f370a80f04df642291220a9b37b5574f
Glenn 'devalias' Grant <glenn@devalias.net>
2023-07-07 13:10:41 +1000

    [system] env: add zoxide _ZO_ECHO=1

--- system/env.zsh
+++ system/env.zsh
@@ -8,3 +8,9 @@ if [[ $EUID -eq 0 ]]; then
 else
   export EDITOR='subl'
 fi
+
+# zoxide is a smarter cd command, inspired by z and autojump.
+# https://github.com/ajeetdsouza/zoxide
+if (( $+commands[zoxide] )); then
+  export _ZO_ECHO=1
+fi
