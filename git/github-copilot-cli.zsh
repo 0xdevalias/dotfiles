17d962105166444ad59b576ccd9676f585bf86f3
Glenn 'devalias' Grant <glenn@devalias.net>
2023-11-12 10:32:13 +1100

    [git] github-copilot-cli: replace old github-copilot-cli private beta with new gh-copilot gh extension

--- git/github-copilot-cli.zsh
+++ git/github-copilot-cli.zsh
@@ -1,6 +1,25 @@
-# github-copilot-cli (https://www.npmjs.com/package/@githubnext/github-copilot-cli)
-if (( $+commands[github-copilot-cli] ))
-then
-  echo "Loading github-copilot-cli.."
-  eval "$(github-copilot-cli alias -- "$0")"
+# GitHub Copilot CLI (https://github.com/github/gh-copilot)
+# Check if the gh copilot extension is installed and load aliases accordingly
+# TODO: potentially refactor/improve this to work with the new gh copilot extension better
+#   see https://github.com/github/gh-copilot/issues/5
+if gh extension list | grep -q 'github/gh-copilot'; then
+  echo "Loading gh copilot extension aliases.. (??, git?, gh?)"
+
+  # Function to handle general shell command suggestions
+  copilot_shell_suggest() {
+    gh copilot suggest -t shell "$@"
+  }
+  alias '??'='copilot_shell_suggest'
+
+  # Function to handle Git command suggestions
+  copilot_git_suggest() {
+    gh copilot suggest -t git "$@"
+  }
+  alias 'git?'='copilot_git_suggest'
+
+  # Function to handle GitHub CLI command suggestions
+  copilot_gh_suggest() {
+    gh copilot suggest -t gh "$@"
+  }
+  alias 'gh?'='copilot_gh_suggest'
 fi
--- git/install.sh
+++ git/install.sh
@@ -10,16 +10,15 @@ check_installed brew
 require_installed_brew gh                          # https://github.com/cli/cli
 require_installed_brew git-delete-merged-branches  # https://github.com/hartwork/git-delete-merged-branches
 
-if (( $+commands[npm] )); then
-  npm install -g @githubnext/github-copilot-cli  # https://www.npmjs.com/package/@githubnext/github-copilot-cli
-else
-  echo "  npm not found, skipping installation of packages"
-fi
-
 # Authenticate the GitHub CLI + set up SSH key
 if (( $+commands[gh] )); then
   if ! gh auth status >/dev/null 2>&1; then
     echo "  GitHub CLI (gh) not authenticated.. please login now"
     gh auth login
+  fi
+
+  # https://github.com/github/gh-copilot
+  if ! gh extension list | grep -q 'github/gh-copilot'; then
+    gh extension install github/gh-copilot
   fi
 fi
