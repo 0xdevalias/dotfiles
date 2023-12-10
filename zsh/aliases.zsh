66a010f3f9cbabf64a7e4374961e62e371da55f6
Glenn 'devalias' Grant <glenn@devalias.net>
2023-12-11 09:08:04 +1100

    [zsh] aliases: add dotfiles-search-commits / _dotfiles-search-commits

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,13 @@
 alias reload!='. ~/.zshrc'
 alias dotfiles-edit='subl $ZSH'
+
+_dotfiles-search-commits() {
+  if [[ $# -eq 0 ]]; then
+    echo "No search term provided. Showing recent commits." >&2
+    git -C $ZSH log --oneline
+  else
+    git -C $ZSH log --oneline --grep "$@"
+  fi
+}
+alias dotfiles-search-commits='_dotfiles-search-commits'
+
