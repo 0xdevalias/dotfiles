c437d86749f7b28d71cfeffe873052965a65776b
Glenn 'devalias' Grant <glenn@devalias.net>
2023-12-17 23:41:32 +1100

    [system] env.zsh: revert $EDITOR workaround when running as root due to subl bug
    reverts eb839175873a17341339d535fb85018acef5dbab

--- system/env.zsh
+++ system/env.zsh
@@ -1,13 +1,4 @@
-# Note: I originally added this customisation due to an issue with subl when running under both root and non-root simultaneously:
-#   https://github.com/sublimehq/sublime_text/issues/3897#issuecomment-1522599530
-#
-# Though according to the following comment, that bug will be fixed as of Sublime Text build 4148 (dev build as of 14 Mar 2023):
-#   https://github.com/sublimehq/sublime_text/issues/3897#issuecomment-1522628513
-if [[ $EUID -eq 0 ]]; then
-  export EDITOR='vi'
-else
-  export EDITOR='subl'
-fi
+export EDITOR='subl'
 
 # zoxide is a smarter cd command, inspired by z and autojump.
 # https://github.com/ajeetdsouza/zoxide
