67abde2f2de5da8f16f892f9bd8258ea690832f6
Glenn 'devalias' Grant <glenn@devalias.net>
2024-03-22 14:48:14 +1100

    [git] github-copilot-cli: refactor gh copilot cli aliases to use the new v1.0 alias helpers
    see:
    
    - https://github.com/github/gh-copilot/issues/5#issuecomment-2013140664
    - https://github.blog/changelog/2024-03-21-github-copilot-general-availability-in-the-cli/

--- git/github-copilot-cli.zsh
+++ git/github-copilot-cli.zsh
@@ -3,23 +3,28 @@
 # TODO: potentially refactor/improve this to work with the new gh copilot extension better
 #   see https://github.com/github/gh-copilot/issues/5
 if gh extension list | grep -q 'github/gh-copilot'; then
-  echo "Loading gh copilot extension aliases.. (??, git?, gh?)"
+  echo "Loading gh copilot extension aliases.. (ghce, ghcs, ??, git?, gh?)"
+
+  eval "$(gh copilot alias -- zsh)"
 
   # Function to handle general shell command suggestions
   copilot_shell_suggest() {
-    gh copilot suggest -t shell "$@"
+    # gh copilot suggest -t shell "$@"
+    ghcs "$@"
   }
   alias '??'='copilot_shell_suggest'
 
   # Function to handle Git command suggestions
   copilot_git_suggest() {
-    gh copilot suggest -t git "$@"
+    # gh copilot suggest -t git "$@"
+    ghcs -t git "$@"
   }
   alias 'git?'='copilot_git_suggest'
 
   # Function to handle GitHub CLI command suggestions
   copilot_gh_suggest() {
-    gh copilot suggest -t gh "$@"
+    # gh copilot suggest -t gh "$@"
+    ghcs -t gh "$@"
   }
   alias 'gh?'='copilot_gh_suggest'
 fi
