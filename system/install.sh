0b0a563e226a844a9decd6309d3ff819433c6378
Glenn 'devalias' Grant <glenn@devalias.net>
2024-12-28 11:31:30 +1100

    [system] install: add csvlens

--- system/install.sh
+++ system/install.sh
@@ -65,6 +65,7 @@ require_installed_brew "cascadia"                    # Go cascadia package comma
 require_installed_brew "fx"                          # Terminal JSON viewer: https://fx.wtf
 require_installed_brew "dasel"                       # JSON, YAML, TOML, XML, and CSV query and modification tool: https://github.com/TomWright/dasel
 require_installed_brew "gron"                        # Make JSON greppable: https://github.com/tomnomnom/gron
+require_installed_brew "csvlens"                     # Command line csv viewer: https://github.com/YS-L/csvlens
 require_installed_brew "ast-grep"                    # A CLI tool for code structural search, lint and rewriting. Written in Rust: https://github.com/ast-grep/ast-grep
 require_installed_brew "git-delta"                   # Syntax-highlighting pager for git and diff output: https://github.com/dandavison/delta
 require_installed_brew "difftastic"                  # Diff that understands syntax: https://github.com/Wilfred/difftastic
