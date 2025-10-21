d561bb80b9ede3ab8c5fbfdc8c99eae178476608
Glenn 'devalias' Grant <glenn@devalias.net>
2025-10-22 09:26:52 +1100

    [system] env: integrate batman for bat manpage viewing
    Adds support for bat-extras' batman command to set MANPAGER and MANROFFOPT via batman integration if available, falling back to manually configuring MANPAGER to use bat otherwise.

--- system/env.zsh
+++ system/env.zsh
@@ -11,6 +11,15 @@ fi
 # A cat(1) clone with wings.
 # https://github.com/sharkdp/bat
 if (( $+commands[bat] )); then
-  # https://github.com/sharkdp/bat#man
-  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+  # Bash scripts that integrate bat with various command-line tools (includes the batman command)
+  # https://github.com/eth-p/bat-extras
+  if (( $+commands[batman] )); then
+    # Export MANPAGER and MANROFFOPT from batman integration
+    # (sets up bat as the manpager automatically)
+    eval "$(batman --export-env)"
+  else
+    # Fallback: manually configure bat as the manpager
+    # https://github.com/sharkdp/bat#man
+    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+  fi
 fi
