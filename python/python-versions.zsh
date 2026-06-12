1a5784e3d378c0494459fb54331243489477da3d
Glenn 'devalias' Grant <glenn@devalias.net>
2026-06-12 19:03:10 +1000

    [python] versions: disable Python 2 + update Python 3 to 3.14.6
    - disable installation of legacy/unsupported Python 2.x
    - update the default Python 3.x version from 3.10.2 to 3.14.6
    - keep Python 3 as the global default

--- python/python-versions.zsh
+++ python/python-versions.zsh
@@ -1,4 +1,4 @@
 # See https://www.python.org/downloads/
-export PYTHON_2X_VER="2.7.18"
-export PYTHON_3X_VER="3.10.2"
+export PYTHON_2X_VER="" # Python 2 is end-of-life and unsupported
+export PYTHON_3X_VER="3.14.6"
 export PYTHON_GLOBAL_VER="3x" # 2x or 3x
